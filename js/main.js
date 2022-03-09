c816b2e5e3ac4c5b2bb587aa213278c661e8d91c
ouivve <>
2022-03-09 16:25:21 +0900

    15. 순차적 애니메이션 - 순차적으로 요소 보이기

--- js/main.js
+++ js/main.js
@@ -37,3 +37,11 @@ window.addEventListener(
     }
   }, 300)
 );
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
