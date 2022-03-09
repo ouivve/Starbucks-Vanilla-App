12ca3ed3facae8c17f001e3003c571768288f12b
ouivve <>
2022-03-09 14:31:50 +0900

    11. 헤더와 드롭다운 메뉴 - 전역배지(GSAP) (1)

--- js/main.js
+++ js/main.js
@@ -14,3 +14,9 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener("scroll", function () {
+  console.log("scroll");
+});
