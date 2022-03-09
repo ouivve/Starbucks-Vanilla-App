cb21a5de24fa98377a5e7e37c026cc3d8120bb7f
ouivve <>
2022-03-09 14:47:42 +0900

    12. 헤더와 드롭다운 메뉴 - 전역배지(GSAP) (2)

--- js/main.js
+++ js/main.js
@@ -17,6 +17,24 @@ searchInputEl.addEventListener("blur", function () {
 
 const badgeEl = document.querySelector("header .badges");
 
-window.addEventListener("scroll", function () {
-  console.log("scroll");
-});
+window.addEventListener(
+  "scroll",
+  // _.throttle(함수, 시간)
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // 배지 숨기기
+      // gsap.to(요소, 지속시간, 옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // 배지 보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
