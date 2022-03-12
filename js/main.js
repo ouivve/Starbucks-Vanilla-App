0451a0a8906e067fabeb3b9261ebf2d8d73046a9
ouivve <ouivve@gmail.com>
2022-03-12 16:57:19 +0900

    20, 21. 요소 슬라이드 - 프로모션 이미지 슬라이드

--- js/main.js
+++ js/main.js
@@ -52,3 +52,22 @@ new Swiper(".notice-line .swiper", {
   autoplay: true,
   loop: true,
 });
+
+new Swiper(".promotion .swiper", {
+  direction: "horizontal", // default
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
