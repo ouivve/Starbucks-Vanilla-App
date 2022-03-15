6545b77352ab2a56be39cf19f9a8112fd5cdf88f
ouivve <ouivve@gmail.com>
2022-03-16 00:02:10 +0900

    25. 유튜브 영상 배경 - 반복 애니메이션

--- js/main.js
+++ js/main.js
@@ -86,3 +86,29 @@ promotionToggleBtn.addEventListener("click", function () {
     promotionEl.classList.remove("hide");
   }
 });
+
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    {
+      // 옵션
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: delay,
+    }
+  );
+}
+
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 15);
+floatingObject(".floating3", 1.5, 20);
