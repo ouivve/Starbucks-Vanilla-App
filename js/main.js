910dc9dddb6d98ad797e8be04510b5637c7c2da1
ouivve <ouivve@gmail.com>
2022-03-17 11:39:41 +0900

    30. 다중 요소 슬라이드

--- js/main.js
+++ js/main.js
@@ -72,6 +72,18 @@ new Swiper(".promotion .swiper", {
   },
 });
 
+new Swiper(".awards .swiper", {
+  direction: "horizontal",
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: ".awards .swiper-prev",
+    nextEl: ".awards .swiper-next",
+  },
+});
+
 const promotionEl = document.querySelector(".promotion");
 const promotionToggleBtn = document.querySelector(".toggle-promotion");
 let isHidePromotion = false;
