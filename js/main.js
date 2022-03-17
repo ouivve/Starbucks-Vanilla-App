cc5e963dd497cddb2b53d466f27fe859865658a5
ouivve <ouivve@gmail.com>
2022-03-17 12:00:16 +0900

    푸터

--- js/main.js
+++ js/main.js
@@ -134,3 +134,6 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear();
